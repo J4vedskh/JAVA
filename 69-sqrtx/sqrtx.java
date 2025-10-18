98f59f3eacf176e22d4c4b31185c5d5196d5fca3
JavedShaikh <95138376+J4vedskh@users.noreply.github.com>
2025-10-18 22:03:00 +0530

    Time: 1 ms (85.42%) | Memory: 41.2 MB (31.93%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,25 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) return x; 
-        int left = 1;
-        int right = x;
-        int ans = 0;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-           
-            long sq = (long) mid * mid;
-            
-            if (sq == x) {
-                return mid;
-            } else if (sq < x) {
-                ans = mid;        // mid is a potential answer
-                left = mid + 1;   // move right to find a bigger value
-            } else {
-                right = mid - 1;  // move left
-            }
+        if (x == 0) return 0;
+        long r = x;
+        while (r * r > x) {
+            r = (r + x / r) / 2;
         }
-        return ans;
+        return (int) r;
     }
 }
